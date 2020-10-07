46193d42e7d8a0a3c669746a7640214fc5e64991
Diego <diegosferas@gmail.com>
2020-10-07 00:56:02 -0500

    Correcion del bug que se daniaba la animacion, ademas de unas mejoras en el codigo y en el rendimiento, crear otro componente para cada section y otro para cada item

--- src/app/components/common/aside/aside.component.ts
+++ src/app/components/common/aside/aside.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { AsideService } from './aside.service';
 import { asideSections } from './aside-default';
@@ -10,42 +10,31 @@ import { asideClosed } from '../../../reducers/aside/aside.actions';
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.scss']
+  styleUrls: ['./aside.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AsideComponent implements OnInit {
+export class AsideComponent {
 
-  @ViewChild('sidenav', { static: false }) sidenav: MatSidenav;
+  @ViewChild('sidenav') sidenav: MatSidenav;
 
-  opened: boolean;
-  //opened2$: Observable<boolean>;
-
+  showAside = false;
   listItems = asideSections;
 
-  constructor(private asideS: AsideService, private store: Store<State>) {
-    this.store.select(data => data.asideState.opened).subscribe(c => { if (c) this.onToggleAside(); });
+  constructor(private asideS: AsideService, private store: Store<State>, private cdk: ChangeDetectorRef) {
+    this.store.select(data => data.asideState.opened).subscribe(c => this.onToggleAside(c));
   }
 
-  ngOnInit(): void {
-
+  onToggleAside(c: boolean) {
+    this.showAside = c;
+    c && this.sidenav.open();
+    this.cdk.markForCheck();
   }
 
-  /** Abre o cierra el aside menu, agrega una clase para abrir el menu */
-  onToggleAside = () => {
-    this.sidenav.toggle();
-    document.querySelector('mat-sidenav-container').classList.toggle('show-aside');
-  }
-
-  /** Al cerrarce el Aside Menu */
-  closeAside = () => {
-    document.querySelector('mat-sidenav-container').classList.remove('show-aside');
-    document.body.style.overflow = '';
-
+  closeAside() {
+    if (!this.showAside) { return; }
     this.store.dispatch(asideClosed({ asideState: { opened: false } }));
   }
 
-  openAside = () => {
-    document.querySelector('mat-sidenav-container').classList.add('show-aside');
-    document.body.style.overflow = 'hidden';
-  }
+  openAside = () => { }
 
 }
